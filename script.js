69b70c027e57f24548af20c17c0ac563f9c4a2d4
GeorgiosKallarogiannis <g.kallarogiannis@its-stuttgart.de>
2025-02-13 23:29:04 +0100

    Add javascript to the project
    The script is used to dynamically change html elements on the page.

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".container");
+const divs = document.querySelectorAll(".cell");
+
+container.addEventListener("mouseover", (e) => {
+    
+}, { capture: true });
