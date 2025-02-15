7197791d9772280ba684e5f4c3a9468faa539920
GeorgiosKallarogiannis <g.kallarogiannis@its-stuttgart.de>
2025-02-15 20:17:21 +0100

    Take user input
    We use a function, that takes the user input and creates
    a square field out of it.

--- script.js
+++ script.js
@@ -2,5 +2,19 @@ const container = document.querySelector(".container");
 const divs = document.querySelectorAll(".cell");
 
 container.addEventListener("mouseover", (e) => {
-    
-}, { capture: true });
+    if (e.target.className === "cell") {
+        e.target.style.backgroundColor = "black";
+    }
+});
+
+function createSketchPadField() {
+    while (true) {
+        const userInput = Number(window.prompt("Type a number between 1 and 100:"));
+
+        if ((userInput > 0) && (userInput < 101)) {
+            break;
+        } else {
+            continue;
+        }
+    }
+}
