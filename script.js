493296c20a41e93299d718c71e57dc3fa51fe92b
GeorgiosKallarogiannis <g.kallarogiannis@its-stuttgart.de>
2025-02-15 22:05:48 +0100

    Add function that sizes the field
    To get a square scratch pad, a function is needed to set a specific
    width to the container.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const container = document.querySelector(".container");
-const divs = document.querySelectorAll(".cell");
 
 container.addEventListener("mouseover", (e) => {
     if (e.target.className === "cell") {
@@ -8,8 +7,10 @@ container.addEventListener("mouseover", (e) => {
 });
 
 function createSketchPadField() {
+    let userInput;
+
     while (true) {
-        const userInput = Number(window.prompt("Type a number between 1 and 100:"));
+        userInput = Number(window.prompt("Type a number between 1 and 100:"));
 
         if ((userInput > 0) && (userInput < 101)) {
             break;
@@ -17,4 +18,33 @@ function createSketchPadField() {
             continue;
         }
     }
+
+    removeAllChildren(container); 
+
+    const childDiv = document.createElement("div");
+    childDiv.className = "cell";
+
+    for (let i = userInput ** 2; i > 0; i--) {
+        container.appendChild(childDiv.cloneNode());
+    }
+
+    console.log(container.offsetWidth);
+    
+    sizePadField(container, userInput);
+}
+
+function removeAllChildren(parent) {
+    const parentElement = parent;
+
+    while (parentElement.firstChild) {
+        parentElement.removeChild(parentElement.firstChild);
+    }
+}
+
+function sizePadField(container, rows) {
+    const childWidth = container.firstChild.offsetWidth;
+    const newContainerWidth = childWidth * rows;
+    
+
+    container.style.width = `${newContainerWidth}px`; 
 }
